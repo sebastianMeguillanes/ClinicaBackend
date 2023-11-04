188f4452b08c424a920d30c0979d88d0963a9685
NataliaEspinozaCode <natalia.espinoza@ucb.edo.bo>
2023-11-03 20:38:38 -0400

    s2

--- src/v1/routes/histClinicaRoutes.js
+++ src/v1/routes/histClinicaRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const histClinicaController = require("../../controllers/histClinicaController");
+const { radiografia, radiografiaImage } = require('../../controllers/histClinicaController');
+
 router
     .get("/", histClinicaController.getAllHistClinica)
     .get("/:id", histClinicaController.getHistClinicaById)
-    .post("/", histClinicaController.createHistClinica)
+    .post("/", histClinicaController.createHistClinica)   
+    .post('/radiografia', radiografia.single(), radiografiaImage)
     .put("/:id", histClinicaController.updateHistClinica)
     .delete("/:id",histClinicaController.deleteHistClinica);   
     
