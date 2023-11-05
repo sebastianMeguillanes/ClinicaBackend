71bd8a627a0c73e4bbc0f00151649e0164f993ed
NataliaEspinozaCode <natalia.espinoza@ucb.edo.bo>
2023-11-05 12:41:51 -0400

    agenda

--- src/service/agendaService.js
+++ src/service/agendaService.js
@@ -4,7 +4,7 @@ const db = require('../database/db');
 const getAll = async ()=> {
     try {
       const response = await db.query(
-        'select * from agenda;'
+        "SELECT a.nombre AS paciente_nombre, a.ci AS paciente_ci, a.fecha_hora, a.descripcion, CONCAT(p.nombre, ' ', p.apellido) AS doctor_nombre_apellido FROM agenda a INNER JOIN doctor d ON a.id_doctor = d.id_doctor INNER JOIN persona p ON d.id_persona = p.id_persona;"
       );
       return response.rows;
     } catch (error) {
@@ -13,14 +13,14 @@ const getAll = async ()=> {
     }
   };
 
-  // Se busca agenda mediante id
-const getOne = async (agendaId) => {
+  // Se busca agenda mediante id doctor
+const getOne = async (doctorId) => {
     try {
       const response = await db.query(
-        'select * from agenda where id_agenda = $1;',
-        [agendaId]
+        "SELECT a.nombre AS paciente_nombre, a.ci AS paciente_ci, a.fecha_hora, a.descripcion, CONCAT(p.nombre, ' ', p.apellido) AS doctor_nombre_apellido FROM agenda a INNER JOIN doctor d ON a.id_doctor = d.id_doctor INNER JOIN persona p ON d.id_persona = p.id_persona where a.id_doctor = $1;",
+        [doctorId]
       );
-      return response.rows[0];
+      return response.rows;
     } catch (error) {
       console.error(error);
       throw error;
@@ -49,6 +49,41 @@ const createNew = async (AgendaData) => {
       throw error;
     }
   };
+
+  // Actualizar un tratamiento mediante el id 
+const updateOne = async (agendaId, AgendaData) => {
+    try {
+        const {
+            nombre,
+            ci,
+            fecha_hora,
+            descripcion,
+            id_doctor
+          } = AgendaData;
+  
+      const response = await db.query(
+        'UPDATE agenda SET nombre = $1, ci = $2, fecha_hora = $3, descripcion = $4, id_doctor = $5 WHERE id_agenda = $6',
+        [nombre,ci,fecha_hora,descripcion,id_doctor,agendaId]
+      );
+  
+      return response.rows;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+
+  // Eliminar un agenda por su ID
+const deleteOne = async (agendaId) => {
+    try {
+      const response = await db.query('DELETE FROM agenda WHERE id_agenda = $1;', [agendaId]);
+      
+      return response.rows;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
   
 
 
@@ -56,5 +91,7 @@ const createNew = async (AgendaData) => {
 module.exports = {
     getAll,
     getOne,
-    createNew
+    createNew,
+    updateOne,
+    deleteOne
   };
